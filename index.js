2ff1177513356c8a2cbf42eefa328cfff00b7862
Zaheer <50898382+zaheeruddinfaiz@users.noreply.github.com>
2019-09-30 12:49:30 +0500

    Prepared app for Heroku Deployment

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const child_process = require('child_process');
-
+const express=require('express')
+const app = express()
+app.get('/',async(req,res)=>{
+    const result = await run_python_script();
+    res.send(result);
+})
 async function run_python_script() {
-    new Promise()
     return new Promise((resolve, reject)=>{
         stream = child_process.spawn('python', ['scraper.py'], { shell: true });
         
         stream.stdout.on('data',(data)=>{
             console.log('This is'+data.toString());
-            resolve(data)
+            resolve(data.toString())
         })
         stream.stderr.on('data',(error)=>{
             console.log(error.toString())
@@ -15,6 +19,4 @@ async function run_python_script() {
         })        
     })
 }
-run_python_script().then((console.log('Data'))).catch((error)=>{
-    console.log('This'+error.toString())
-})
+app.listen(3000,'localhost');
